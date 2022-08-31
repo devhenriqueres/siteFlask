e4323413b00583bcdbd9a939847085090b8e2481
Henrique <rickresende03@gmail.com>
2022-08-31 17:16:55 -0300

    pag flask

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Criar a primeira página do site
 # route _ caminho(meusite.com/)
-
 # função _ o que você quer exibir naquela página
+# template
+
 @app.route("/")
 def homepage():
-    return "Esse é meu site"
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template('contatos.html')
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template('usuarios.html', nome_usuario=nome_usuario)
+
 
 # Colocar o site no ar
 if __name__ == '__main__':
